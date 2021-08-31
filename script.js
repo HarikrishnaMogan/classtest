88fe9ca441dd96d01882bb864d2268749b1c4927
HarikrishnaMogan <harikrishss437@gmail.com>
2021-08-31 19:19:10 +0530

    added PUT iin script.js

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ function displaydata(users)
     <p class="name">${users.name}</p>
     <p class="id">${users.createdAt}</p>
     <button class="delete" onclick="deleteid(${users.id})">Delete</button>
+    <button class="delete" onclick="edit(${users.id})">Edit</button>
+    <div class="edit edit${users.id}">
+    <input class="editname${users.id}" type="text" placeholder="name">
+    <button class="delete" onclick="editsubmit(${users.id})">Ok</button>
+    </div>
     </div>
     </div>
     `; 
@@ -56,4 +61,29 @@ function addprofile()
 
   }).then(data=>data.json());
   setTimeout(()=>{ refresh();},800)
+}
+ function edit(id)
+{
+  let edit = document.querySelector(`.edit${id}`);
+  edit.style.display="block";
+} 
+function editsubmit(id)
+{
+  let name = document.querySelector(`.editname${id}`);
+  fetch(`https://611f26469771bf001785c730.mockapi.io/users/${id}`,
+  {
+    method:"PUT",
+    headers:{
+      "Content-type":"application/json"
+    },
+    body:JSON.stringify({
+      name:name.value,
+    })
+
+  })
+  .then(data=>data.json());
+  setTimeout(()=>{ refresh();},800)
+  let edit = document.querySelector(".edit");
+  edit.style.display="none";
+  
 }
